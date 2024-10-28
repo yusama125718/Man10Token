e847e0e8255e3c5a7697d9ec23e2ef090a1d4487
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2024-10-29 00:35:10 +0900

    Initial commit

---
+++ src/main/java/yusama125718/man10Token/Man10Token.java
@@ -0,0 +1,17 @@
+package yusama125718.man10Token;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10Token extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
